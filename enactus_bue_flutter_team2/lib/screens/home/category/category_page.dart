91ffd0e38275c8844f446ee1ebd35daf8b1ae40d
XZSPACE\XZSPACE <ahmedbarakat911@outlook.com>
2025-02-11 15:13:39 +0200

    edit in category page 15:13 / 11.2.2025

--- enactus_bue_flutter_team2/lib/screens/home/category/category_page.dart
+++ enactus_bue_flutter_team2/lib/screens/home/category/category_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class CategoryPage extends StatelessWidget {
   const CategoryPage({super.key});
@@ -6,51 +7,206 @@ class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor:  const Color.fromARGB(255, 247, 247, 247),
       body: Column(
         children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.blue,
-                        Colors.purple,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                    borderRadius: BorderRadius.circular(35),
-                  ),
-                  height: 200,
-                  width: double.infinity,
-                  child: Image.asset("assets/images/des.png"),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue,
+                  Colors.purple,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              
+            ),
+            height: 120,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.only(top : 45 , left: 10 , right: 10),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.purple,
                 ),
-                Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  hintStyle: TextStyle(
+                    color: Colors.purple,
+                  ),
+                  suffixIcon: Icon(Iconsax.search_favorite),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Stack(
+            children: [
+              Container(
+                height: 150,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                   
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 140),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: const Color.fromARGB(136, 177, 21, 255),
+                      
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Introducing",
+                            style:
+                                TextStyle(color: Colors.grey, fontSize: 14),
                           ),
-                          height: 150,
+                          Text(
+                            "Sedan",
+                            style: TextStyle(
+                              fontFamily: "Coolvetica",
+                              fontSize: 18,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            height: 30,
+                            width: 90,
+                            padding: EdgeInsets.only(
+                                top: 4, bottom: 4, left: 17, right: 4),
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 11),
+                              child: Text(
+                                "View",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 150,
+                top: 1,
+                child: SizedBox(
+                  height: 200,
+                        child: Image(
+                          image: AssetImage(
+                            "assets/images/6.png",
+                          ),
+                          height: 300,
                           width: 300,
                         ),
                       ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+              ),
+              
+            ], 
           ),
+          SizedBox(height: 10,),
+        Stack(
+          children: [
+            Container(
+              height: 150,
+              width: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                 
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Introducing",
+                          style:
+                              TextStyle(color: Colors.grey, fontSize: 14),
+                        ),
+                        Text(
+                          "Sedan",
+                          style: TextStyle(
+                            fontFamily: "Coolvetica",
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          height: 30,
+                          width: 90,
+                          padding: EdgeInsets.only(
+                              top: 4, bottom: 4, left: 17, right: 4),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 11),
+                            child: Text(
+                              "View",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              right: 150,
+              top: 1,
+              child: SizedBox(
+                height: 200,
+                      child: Image(
+                        image: AssetImage(
+                          "assets/images/6.png",
+                        ),
+                        height: 300,
+                        width: 300,
+                      ),
+                    ),
+            ),
+            
+          ],
+          
+        ),  
         ],
+        
       ),
     );
   }
 }
+
